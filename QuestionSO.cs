765b32655ef95267d423fb51094c9b53ccc65117
Kyrune <jn-01@centurytel.net>
2021-10-08 13:33:34 -0500

    Add ScriptableObject/CreateAssetMenu

--- QuestionSO.cs
+++ QuestionSO.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestionSO : MonoBehaviour
+[CreateAssetMenu()]
+public class QuestionSO : ScriptableObject
 {
 
 }
