4df01efd05b47670d359fdf6f135a256eb1d4ecd
Kyrune <jn-01@centurytel.net>
2021-10-14 14:03:15 -0500

    Add DisplayAnswer method

--- Quiz.cs
+++ Quiz.cs
@@ -10,4 +10,25 @@ public class Quiz : MonoBehaviour
     {
         questionText.text = question.GetQuestion();
     }
+
+    void DisplayAnswer(int index)
+    {
+        Image buttonImage;
+
+        if (index == currentQuestion.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+            scoreKeeper.IncrementCorrectAnswers();
+        }
+        else
+        {
+            correctAnswerIndex = currentQuestion.GetCorrectAnswerIndex();
+            string correctAnswer = currentQuestion.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was;\n" + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+    }
 }
