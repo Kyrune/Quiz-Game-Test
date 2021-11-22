17219b161f9df970183c77796ba92f20cb080b08
Kyrune <jn-01@centurytel.net>
2021-11-22 13:05:19 -0600

    Create Scorekeeper

--- EndScreen.cs
+++ EndScreen.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class EndScreen : MonoBehaviour
 {
+    ScoreKeeper scoreKeeper;
+
     void Awake()
     {
-        
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
 
