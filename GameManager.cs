cd9b0db628169e74b59081440a24d8b534c88fdd
Kyrune <jn-01@centurytel.net>
2021-11-22 13:13:02 -0600

    Create quiz

--- GameManager.cs
+++ GameManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    Quiz quiz;
 
     void Awake() 
     {
-        
+        quiz = FindObjectOfType<Quiz>();
     }
 
     void Start()
     {
-        
+        quiz.gameObject.SetActive(true);
     }
 
     void Update()
