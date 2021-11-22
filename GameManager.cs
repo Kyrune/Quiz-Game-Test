6457d22a1ac2999ae7c5c3d84db6256e40c7cfac
Kyrune <jn-01@centurytel.net>
2021-11-22 13:13:31 -0600

    Create endScreen

--- GameManager.cs
+++ GameManager.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     Quiz quiz;
+    EndScreen endScreen;
 
     void Awake() 
     {
         quiz = FindObjectOfType<Quiz>();
+        endScreen = FindObjectOfType<EndScreen>();
     }
 
     void Start()
     {
         quiz.gameObject.SetActive(true);
+        endScreen.gameObject.SetActive(false);
     }
 
     void Update()
